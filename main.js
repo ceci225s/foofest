35be3cfce86682c8c7387491b627e01eb479c8d9
asef0024 <asef0024@stud.kea.dk>
2022-05-12 10:45:21 +0200

    comments

--- main.js
+++ main.js
@@ -1,1 +1,54 @@
-import "./style.css";
+"use strict";
+import "./sass/style.scss";
+
+window.addEventListener("DOMContentLoaded", start);
+
+async function start() {
+  await loadBandJson();
+  await loadScheduleJson();
+}
+
+//------------------------ FETCH ALL DATA
+
+//Fetch bands
+async function loadBandJson() {
+  const bands = await fetch("https://foo-techno-fest.herokuapp.com/bands", {
+    method: "GET",
+  });
+  const bandJson = await bands.json();
+  console.log(bandJson);
+  // buildBandList();
+}
+
+//Fetch schedule
+async function loadScheduleJson() {
+  const schedule = await fetch(
+    "https://foo-techno-fest.herokuapp.com/schedule",
+    {
+      method: "GET",
+    }
+  );
+  const scheduleJson = await schedule.json();
+  console.log(scheduleJson);
+}
+
+//------------------------ APP
+//Camping spots
+const availableSpots = await fetch(
+  "https://foo-techno-fest.herokuapp.com/available-spots",
+  {
+    method: "GET",
+  }
+);
+const availableSpotsJson = await availableSpots.json();
+console.log(availableSpotsJson);
+
+//------------------------ SORT
+
+//------------------------ FILTER
+
+//------------------------ SHOW BANDS
+
+//------------------------ SHOW SCHEDULE
+
+//------------------------
